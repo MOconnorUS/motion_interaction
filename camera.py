ccd3ee35742da9c801b7214f0c54fd94690356df
Matthew <oconnormattc@gmail.com>
2025-10-25 14:21:27 -0400

    Pushing initial skeleton for repository

---
+++ camera.py
@@ -0,0 +1,17 @@
+"""
+The camera file is reserved for all functions related to the camera recording, opencv, and mediapipe.
+
+When you are programming in python be sure to use type hinting as it clears up confusion between co-developers
+and readers. If you are not familiar with type hinting see the following example.
+
+--- Type Hinting ---
+To type hint you must provide the type of the input parameters of a function as well as the return type 
+of the function.
+
+Example:
+def foo(x : int, s : str) -> None:
+
+--- Documentation ---
+For the first few lines within each function please use the triple quotes as seen in this comment to record 
+documentation about each function.
+"""
---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+The main file is reserved for bringing together all information from peripheral_interactions, camera, etc. 
+This file can also contain any helper functions to get things to run but it should be kept clean and concise.
+
+When you are programming in python be sure to use type hinting as it clears up confusion between co-developers
+and readers. If you are not familiar with type hinting see the following example.
+
+--- Type Hinting ---
+To type hint you must provide the type of the input parameters of a function as well as the return type 
+of the function.
+
+Example:
+def foo(x : int, s : str) -> None:
+
+--- Documentation ---
+For the first few lines within each function please use the triple quotes as seen in this comment to record 
+documentation about each function.
+"""
